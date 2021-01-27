c19f43886f8b6332955f2a88c2dad8d1923abc05
DELL <kunal.choudhary>
2021-01-27 16:41:36 +0530

    Pushing requirements.txt

--- xtra_webcam.py
+++ xtra_webcam.py
@@ -1,8 +1,17 @@
 import cv2
 from xtra_model import xtra_model
 import math
+import logging
 
-# 0- webcam 
+logger = logging.getLogger('run')
+logger.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter('[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s')
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+
 cap=cv2.VideoCapture(0)
 
 _, image = cap.read()
@@ -40,14 +49,14 @@ while True:
         image = cv2.line(image, neck, lwri, color, thickness) 
         
         angle = getAngle(rwri, neck, lwri)
-        print(angle)
         ang = round(angle, 2)
         message = "Angle : "+str(ang)+" degree"
+        logger.info(message)
         image = cv2.putText(image, message, org, cv2.FONT_HERSHEY_SIMPLEX , 1, (0,0,255), thickness, cv2.LINE_AA)
     else:
 
         message = "Hands not not visible. Make sure both the wrist are visible in the frame."
-        print(message)
+        logger.error(message)
 
     cv2.imshow("capture",frame) 
     if cv2.waitKey(1) & 0xFF==ord('q'):
